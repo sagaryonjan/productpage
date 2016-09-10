7fa0e64dfcbdd49211ae38112910cd9bfe51ede0
sagaryonjan <sagaryonjan015@gmail.com>
2016-09-10 15:02:38 +0545

    change

--- footer.php
+++ footer.php
@@ -55,11 +55,7 @@
 						<?php } ?>
 					</div>
 				<?php endif; ?>
-	           <!-- <div class="menu-social-menu-container"><ul id="social-menu" class="menu"><li id="menu-item-37" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-37"><a href="http://facebook.com/99colorthemes"><i class="fa fa-facebook"></i></a></li>
-				<li id="menu-item-38" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-38"><a href="http://twitter.com/99colorthemes"><i class="fa fa-twitter"></i></a></li>
-				<li id="menu-item-39" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-39"><a href="https://plus.google.com/"><i class="fa fa-google-plus"></i></a></li> 
-				</ul>
-				</div>  -->
+	           
 			</div>       
 		</footer>
 
@@ -68,9 +64,9 @@
 
 		<div class="ts-bottom-footer">
 			<div class="ts-container">
-				<?php productpage_footer_copyright_info(); ?>
-				<!--<p>&copy RainbowNews 2016, All Rights Reserved. Powered By WordPress.</p>
-				<p>Designed By ThemeSpade &spades;</p>-->
+				<?php /*productpage_footer_copyright_info(); */?>
+				<p>&copy RainbowNews 2016, All Rights Reserved. Powered By WordPress.</p>
+				<p>Designed By ThemeSpade &spades;</p>
 			</div>
 		</div>
 
